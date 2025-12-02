605332c86a45e415d2740c0fd10e004c196e0e0f
SurKaa <98200894+surkaa@users.noreply.github.com>
2025-12-02 21:22:01 +0800

    添加解密加密文件的测试用例

--- src-tauri/tests/app_state_test.rs
+++ src-tauri/tests/app_state_test.rs
@@ -47,7 +47,9 @@ mod app_state_test {
         let (encryption, oss, store) = create_store().await;
         let cache = DiaryMemoryCache::new();
 
-        state.load_cache_to_memory(&cache, &encryption, &store, None).await
+        state
+            .load_cache_to_memory(&cache, &encryption, &store, None)
+            .await
             .expect("Failed to load cache");
 
         let cur_diaries = state.list_cached_diaries(&cache).await;
@@ -59,11 +61,47 @@ mod app_state_test {
             .await
             .expect("Failed to sync from OSS");
 
-        state.load_cache_to_memory(&cache, &encryption, &store, None).await
+        state
+            .load_cache_to_memory(&cache, &encryption, &store, None)
+            .await
             .expect("Failed to load cache");
 
         let updated_diaries = state.list_cached_diaries(&cache).await;
         println!("Updated cached diaries: {:?}", updated_diaries);
         assert!(!updated_diaries.is_empty());
+    }
+
+    #[tokio::test]
+    async fn test_decrypt_enc_file() {
+        let state = AppState {};
+        let (encryption, _, _) = create_store().await;
+
+        // 获取本地加密文件路径
+        let enc_file_path = state.get_diary_cache_dir(None);
+        // 读取enc_file_path下的所有加密文件
+        let entries =
+            std::fs::read_dir(&enc_file_path).expect("Failed to read diary cache directory");
+        for entry in entries {
+            let entry = entry.expect("Failed to get directory entry");
+            let path = entry.path();
+            if path.extension().and_then(|s| s.to_str()) == Some("enc") {
+                // 读取得到字节流
+                let enc_data = std::fs::read(&path).expect("Failed to read encrypted file");
+                // 划分nonce和密文
+                let (nonce, enc_data) = enc_data.split_at(12);
+                println!("读取到加密文件: {:?}", path);
+                println!("nonce: {:?}", nonce);
+                println!("enc_data.len: {:?}", enc_data.len());
+                // 解密字节流
+                let decrypted_data = encryption
+                    .decrypt(&enc_data, nonce)
+                    .await
+                    .expect("Failed to decrypt data");
+                println!(
+                    "decrypted_data: {:?}",
+                    String::from_utf8_lossy(&decrypted_data)
+                );
+            }
+        }
     }
 }
