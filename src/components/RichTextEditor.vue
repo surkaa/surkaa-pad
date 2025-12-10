4b0c71b62552bccdefed228621d45a470b38bf28
SurKaa <98200894+surkaa@users.noreply.github.com>
2025-12-10 22:55:46 +0800

    尝试创建富文本编辑器

---
+++ src/components/RichTextEditor.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import {convertFilename2URL} from "../utils/convertFilename2URL.ts";
+import {DiaryManifest} from "../types";
+const {diary} = defineProps<{ diary: DiaryManifest }>();
+</script>
+
+<template>
+  <div class="rich-text-editor">
+  </div>
+</template>
+
+<style scoped lang="scss">
+.rich-text-editor {
+  width: 100%;
+  height: 100%;
+}
+</style>
