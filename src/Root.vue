1b78f56eedf37d0d849aa09c0e536c6336e626f8
SurKaa <98200894+surkaa@users.noreply.github.com>
2025-12-11 22:37:25 +0800

    重构Root.vue，简化刷新禁用逻辑，移除不必要的样式和过渡效果

--- src/Root.vue
+++ src/Root.vue
@@ -1,10 +1,7 @@
 <script setup lang="ts">
-import {useRoute} from "vue-router";
 import {onMounted} from "vue";
 
-const route = useRoute();
-
-const disableRefresh = () => {
+function disableRefresh() {
   document.addEventListener('keydown', function (event) {
     // 阻止 F5 键
     if (event.key === 'F5') {
@@ -27,56 +24,13 @@ const disableRefresh = () => {
     event.preventDefault();
     return false;
   });
-};
+}
 
-onMounted(disableRefresh);
+onMounted(() => {
+  disableRefresh();
+});
 </script>
 
 <template>
-  <router-view v-slot="{ Component }">
-    <transition name="fade-transform" mode="out-in">
-      <component :key="route.path" :is="Component"/>
-    </transition>
-  </router-view>
+  <router-view/>
 </template>
-
-<style scoped lang="scss">
-#app-main {
-  width: 100%;
-  flex: 1;
-  overflow: hidden; // 隐藏动画带来的滚动条
-  background-color: var(--pad-bg-color-400);
-
-  .fade-transform-leave-active,
-  .fade-transform-enter-active {
-    transition: all .3s;
-  }
-
-  .fade-transform-enter {
-    opacity: 0;
-    transform: translateX(-30px);
-  }
-
-  .fade-transform-leave-to {
-    opacity: 0;
-    transform: translateX(30px);
-  }
-
-  //region 滚动条样式
-  &::-webkit-scrollbar {
-    width: 8px;
-    height: 8px;
-  }
-
-  &::-webkit-scrollbar-thumb {
-    background-color: var(--pad-bg-color-300);
-    border-radius: 4px;
-  }
-
-  &::-webkit-scrollbar-track {
-    background-color: var(--pad-bg-color-100);
-  }
-
-  //endregion
-}
-</style>
