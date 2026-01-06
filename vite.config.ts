389ec7085cf142ba8ba62215f5710c51daff4d8c
SurKaa <98200894+surkaa@users.noreply.github.com>
2026-01-06 22:39:28 +0800

    更新vite配置以使用loadEnv加载环境变量，并简化开发服务器配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,25 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// @ts-expect-error process is a nodejs global
-const host = process.env.TAURI_DEV_HOST;
-
-// https://vitejs.dev/config/
-export default defineConfig(async () => ({
-  plugins: [vue()],
-
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
-  clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
-  server: {
-    port: 5173,
-    strictPort: true,
-    host: host || false,
-    hmr: host
-      ? {
-          protocol: "ws",
-          host,
-          port: 5174,
-        }
-      : undefined,
-    watch: {
-      // 3. tell vite to ignore watching `src-tauri`
-      ignored: ["**/src-tauri/**"],
-    },
-  },
-}));
+export default defineConfig(async ({mode}) => {
+    const env = loadEnv(mode, process.cwd());
+    const host = env.VITE_TAURI_DEV_HOST;
+    const hmr = host ? {
+        protocol: "ws",
+        host,
+        port: 5174,
+    } : undefined;
+    return {
+        plugins: [vue()],
+        clearScreen: false,
+        server: {
+            port: 5173,
+            strictPort: true,
+            host: host || false,
+            hmr,
+            watch: {
+                ignored: ["**/src-tauri/**"],
+            },
+        },
+    }
+});
