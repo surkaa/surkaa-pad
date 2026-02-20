64c65634f7b9ee28d0e39200762a8aa635ec121c
SurKaa <98200894+surkaa@users.noreply.github.com>
2026-02-20 12:45:58 +0800

    添加quasar

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,17 @@ import '/src/assets/toast.style.css';
 import router from "./router";
 import clickOutsideDirective from './directives/vClickOutside.ts';
 
+import '@quasar/extras/material-icons/material-icons.css'
+import 'quasar/src/css/index.sass'
+import { Quasar } from 'quasar'
+
 const app = createApp(Root);
 app.use(createPinia());
 app.use(router);
+app.use(Quasar, {
+    plugins: {},
+});
 
-app.directive('click-outside', clickOutsideDirective)
+app.directive('click-outside', clickOutsideDirective);
 
 app.mount("#app");
