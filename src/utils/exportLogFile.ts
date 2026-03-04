b3f0cdb1df689bb1cd22fb6f111badac3eb3a0ee
SurKaa <98200894+surkaa@users.noreply.github.com>
2026-03-04 14:35:47 +0800

    解决在android端无法导出日志的问题

--- src/utils/exportLogFile.ts
+++ src/utils/exportLogFile.ts
@@ -1,14 +1,17 @@
-import {appLogDir, join} from "@tauri-apps/api/path";
+import {BaseDirectory} from "@tauri-apps/api/path";
 import {save} from "@tauri-apps/plugin-dialog";
-import {copyFile} from "@tauri-apps/plugin-fs";
+import {exists, readFile, writeFile} from "@tauri-apps/plugin-fs";
 import {getName} from "@tauri-apps/api/app";
 
-// TODO 导出的文件可能是空文件
 export async function exportLogFile() {
     try {
-        const logDir = await appLogDir();
         const name = await getName();
-        const sourceFile = await join(logDir, `${name}.log`);
+        const sourceFile = `${name}.log`;
+
+        if (!await exists(sourceFile, {baseDir: BaseDirectory.AppLog})) {
+            console.log('日志文件不存在:', sourceFile);
+            return;
+        }
 
         const savePath = await save({
             title: "导出系统日志",
@@ -17,8 +20,12 @@ export async function exportLogFile() {
         });
 
         if (savePath) {
-            await copyFile(sourceFile, savePath);
+            console.log('选择的保存路径:', savePath);
+            const contents = await readFile(sourceFile, { baseDir: BaseDirectory.AppLog });
+            await writeFile(savePath, contents);
             console.log("日志导出成功:", savePath);
+        } else {
+            console.log("用户取消了导出操作");
         }
     } catch (e) {
         console.error("导出失败:", e);
