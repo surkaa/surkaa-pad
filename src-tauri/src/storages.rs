815c1ff4cde0aaa682a518a4e7aeaacb7c12ec98
SurKaa <98200894+surkaa@users.noreply.github.com>
2026-03-15 23:56:59 +0800

    feat: 添加 PathBuf join功能的单元测试

--- src-tauri/src/storages.rs
+++ src-tauri/src/storages.rs
@@ -5,10 +5,27 @@ pub fn remote_manifest_key(diary_id: &str) -> String {
 
 /// 将日记主文件的key解析出日记ID
 pub fn diary_id_from_manifest_key(key: &str) -> Option<String> {
-    key.strip_suffix("/manifest.enc").map(|stripped| stripped.to_string())
+    key.strip_suffix("/manifest.enc")
+        .map(|stripped| stripped.to_string())
 }
 
 /// 日记附件在云存储中的路径
 pub fn remote_attachments_key(diary_id: &str, attachment_filename: &str) -> String {
     format!("{}/{}", diary_id, attachment_filename)
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+
+    #[test]
+    fn test_path_buf_join_multiple() {
+        let root = PathBuf::from("root");
+        let path = root
+            .join("aaa/bbb/aaaa.txt")
+            .to_str()
+            .expect("Failed to convert PathBuf to str")
+            .replace("\\", "/");
+        assert_eq!(&path, "root/aaa/bbb/aaaa.txt");
+    }
 }
