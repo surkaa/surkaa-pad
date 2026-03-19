2ca7a9c957f738c379f67aba07100d0d5934b3be
SurKaa <98200894+surkaa@users.noreply.github.com>
2026-03-19 13:30:04 +0800

    fix: 从文件缓存检查中移除未使用的大小变量

--- src-tauri/src/diaries/diary.rs
+++ src-tauri/src/diaries/diary.rs
@@ -74,8 +74,8 @@ pub async fn get_diary(
         }
     }
 
-    // 检查文件缓存 TODO 考虑去掉SIZE
-    if let Some((size, etag)) = lfc.get(&object_key).await? {
+    // 检查文件缓存
+    if let Some((_size, etag)) = lfc.get(&object_key).await? {
         if etag == metadata.etag() {
             let cache_bytes = lfc.get_data(&object_key).await?;
             let manifest_bytes = crypto.decrypt(&cache_bytes)?;
