9a91d16f4a859f832c72a9551e18b60c1101a80e
SurKaa <98200894+surkaa@users.noreply.github.com>
2026-03-25 22:25:07 +0800

    fix: 更新isEncrypted属性的TODO

--- src/components/editor/extension.ts
+++ src/components/editor/extension.ts
@@ -47,7 +47,7 @@ export interface Extension<N extends HTMLElement = HTMLElement> {
     // 上下文菜单 (windows右键/android长按) -> 返回要显示的菜单按钮列表
     onContextmenu?: (e: MouseEvent, node: N, ctx: ExtensionContext) => MenuButton<N>[];
 
-    // 是否是加密了的附件
+    // 是否是加密了的附件 TODO 这应该算是日记的逻辑
     isEncrypted?: (node: N, ctx: ExtensionContext) => boolean;
 
     // 获取附件的文件名
